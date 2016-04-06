7e24fac61f4bbe013eb646bdcf990e360e75aa51
anoskov <an@gettable.ru>
2016-04-06 15:16:11 +0300

    added string representation for Uptime

---
+++ format.go
@@ -0,0 +1,34 @@
+package sysinfo
+
+import (
+	"bytes"
+	"bufio"
+	"fmt"
+)
+
+func (self *Uptime) Format() string {
+	buf := new(bytes.Buffer)
+	w := bufio.NewWriter(buf)
+	dur := uint64(self.Duration)
+
+	days := dur / (60 * 60 * 24)
+
+	if days != 0 {
+		s := ""
+		if days > 1 {
+			s = "s"
+		}
+		fmt.Fprintf(w, "%d day%s, ", days, s)
+	}
+
+	minutes := dur / 60
+	hours := minutes / 60
+	hours %= 24
+	minutes %= 60
+
+	fmt.Fprintf(w, "%2d:%02d", hours, minutes)
+
+	w.Flush()
+
+	return buf.String()
+}
